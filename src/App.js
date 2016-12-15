1a816df6ad348822c6ccbf4b455e13966250daac
Franck Ernewein <franck.ernewein@gmail.com>
2016-12-15 11:25:40 +0100

    task: display profie picture

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import './Wall.css';
 import React from 'react';
 import { connect } from 'react-redux';
 
-export default connect((posts) => {
-  return {
-    posts
-  };
-})(function Wall(props) {
+function Wall(props) {
   const {posts, permalink} = props;
 
   return <div className="wall">
     {posts.map((post, i) => {
       return <div key={i} className="post">
+        <img src={post.user.profile_picture} alt="" />
         <h2>@{post.user.name}</h2>
         <p>{post.text}</p>
+        <p>{post._source}</p>
         <a className="fullsize" target="_blank" href={permalink(post._id)} />
       </div>;
     })}
   </div>;
-});
+}
+
+export default connect((posts) => {
+  return {
+    posts
+  };
+})(Wall);
